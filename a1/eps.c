88be4c697da3dadc8ffd2e1f9dfb7f1ad770ea6c
Kumar Ayush <cheekujodhpur@gmail.com>
2016-01-19 10:49:21 +0530

    question 1

--- a1/eps.c
+++ a1/eps.c
@@ -3,3 +3,21 @@
  * 
  * Author: Kumar Ayush, 140260016
  */
+
+#include <stdio.h>
+
+int main()
+{
+    float eps_f = 1.0f;
+    float eps_d = 1.0;
+
+    while(1.0f+eps_f > 1.0f)
+        eps_f /= 2.0;
+
+    while(1.0 + eps_d > 1.0)
+        eps_d /= 2.0;
+
+    printf("Epsilon for float: %.17g\n",eps_f);
+    printf("Epsilon for double: %.17g\n",eps_d);
+    return 0;
+}
