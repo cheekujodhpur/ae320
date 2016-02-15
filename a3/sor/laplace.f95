7eb6a6d1fbf3fc2dcde546b96505e365a0b134a3
Kumar Ayush <cheekujodhpur@gmail.com>
2016-02-15 19:20:28 +0530

    checkpoint

--- a3/sor/laplace.f95
+++ a3/sor/laplace.f95
@@ -1,8 +1,9 @@
 PROGRAM LAPLACE
     INTEGER, PARAMETER :: XSIZE=41,YSIZE=41
-    REAL(8), DIMENSION(1:XSIZE,1:YSIZE) :: PHIOLD,PHINEW,ERROR
+    REAL(8), DIMENSION(1:XSIZE,1:YSIZE) :: PHIOLD,PHINEW,ORIPHI,ERROR
     REAL :: MAXDELTA
-    REAL(8) :: ERRORSUM,NSQ=XSIZE*YSIZE
+    REAL(8) :: ERRORSUM,PHISTAR,NSQ=XSIZE*YSIZE
+    REAL :: OMEGA
 
     REAL(8), PARAMETER :: CONVERGENCE = EPSILON(ERRORSUM)
     INTEGER :: ITERATIONS = 0
@@ -17,27 +18,38 @@ PROGRAM LAPLACE
     
     PHINEW(2:XSIZE-1,2:YSIZE-1)=0
 
-    open(10,file='gs_41.csv')
+    ORIPHI(1:XSIZE,1:YSIZE) = PHINEW(1:XSIZE,1:YSIZE)
 
-    GAUSS_ITERATION: DO
-        ITERATIONS = ITERATIONS+1
-        PHIOLD=PHINEW
+    open(10,file='sor_41.csv')
 
-        DO I=2,XSIZE-1
-            DO J=2,YSIZE-1
-                PHINEW(I,J)=0.25*(&
-                    &PHIOLD(I-1,J)+&
-                    &PHIOLD(I+1,J)+&
-                    &PHIOLD(I,J-1)+&
-                    &PHIOLD(I,J+1))
+    OMEGA = 0.0
+
+    HUNT_OMEGA:DO
+        OMEGA = OMEGA+0.1
+        ITERATIONS = 0
+        PHINEW(1:XSIZE,1:YSIZE) = ORIPHI(1:XSIZE,1:YSIZE)
+        GAUSS_ITERATION: DO
+            ITERATIONS = ITERATIONS+1
+            PHIOLD=PHINEW
+
+            DO I=2,XSIZE-1
+                DO J=2,YSIZE-1
+                    PHINEW(I,J)=(0.25*(&
+                        &PHINEW(I-1,J)+&
+                        &PHINEW(I+1,J)+&
+                        &PHINEW(I,J-1)+&
+                        &PHINEW(I,J+1))-PHINEW(I,J))*OMEGA+&
+                        &PHINEW(I,J)
+                END DO
             END DO
-        END DO
-        ERROR = PHIOLD-PHINEW
-        ERRORSUM = SQRT(SUM(ERROR*ERROR)/NSQ)
-        write(10,*) ERRORSUM, ITERATIONS 
-        IF (ERRORSUM < 2*CONVERGENCE) EXIT GAUSS_ITERATION
+            ERROR = PHIOLD-PHINEW
+            ERRORSUM = SQRT(SUM(ERROR*ERROR)/NSQ)
+            if (ITERATIONS>=100) EXIT GAUSS_ITERATION
+         END DO GAUSS_ITERATION
 
-     END DO GAUSS_ITERATION
+         write(10,*) OMEGA, ERRORSUM 
+         if (OMEGA >= 1.0) EXIT HUNT_OMEGA
+    END DO HUNT_OMEGA
    
     close(10)
 END PROGRAM LAPLACE
